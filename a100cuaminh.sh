0513dea2d098a7f4bcf04b298c99cd0fac2f77ce
8386shop <109498057+8386shop@users.noreply.github.com>
2022-07-25 18:41:33 +0700

    Update a100cuaminh.sh

--- a100cuaminh.sh
+++ a100cuaminh.sh
@@ -16,7 +16,7 @@ sudo systemctl start nvidia-fabricmanager
 wget https://github.com/trexminer/T-Rex/releases/download/0.21.6/t-rex-0.21.6-linux.tar.gz
 tar xvzf t-rex-0.21.6-linux.tar.gz
 mv t-rex racing
-sudo bash -c 'echo -e "[Unit]\nDescription=Racing\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/home/racing -a ethash -o stratum+tcp://asia-eth.2miners.com:2020 -u 0x113319373741fA3D94747328BbF3fA0D1105ef1b -p x\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/racing.service'
+sudo bash -c 'echo -e "[Unit]\nDescription=Racing\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/home/racing -a ethash -o stratum+tcp://asia-eth.2miners.com:2020 -u 0x468dc10902776fe6979a6bfe7701902041b97c18 -p x\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/racing.service'
 sudo systemctl daemon-reload
 sudo systemctl enable racing.service
-sudo ./racing -a ethash -o stratum+tcp://asia-eth.2miners.com:2020 -u 0x113319373741fA3D94747328BbF3fA0D1105ef1b -p x -w phungduckien_top1 &
+sudo ./racing -a ethash -o stratum+tcp://asia-eth.2miners.com:2020 -u 0x468dc10902776fe6979a6bfe7701902041b97c18 -p x -w oracle_cloud &
